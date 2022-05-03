b7bf35aa98d330d915aae04e0f49fd3a7aa33f57
marcushurn <marcushurn@hotmail.com>
2022-05-02 18:16:48 -0700

    paginate results

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material";
-import { FunctionComponent, useEffect, useRef } from "react";
+import { Box, Pagination } from "@mui/material";
+import { FunctionComponent, useEffect, useRef, useState } from "react";
 import { DrinkType, SearchResult } from "../types";
 import CoctailRecipe from "./CoctailRecipe";
 import DrindThumbnail from "./DrinkThumbnail";
@@ -14,18 +14,46 @@ interface SearchResultsProps {
  */
 const SearchResults: FunctionComponent<SearchResultsProps> = ({ coctails }) => {
   const listRef = useRef<HTMLDivElement>(null);
+  const [page, setPage] = useState(1);
+  const RESULTSPERPAGE = 10;
   
   useEffect(() => {
     if(listRef.current)
     listRef.current.scrollIntoView({behavior: "smooth", block: "start"});
+
+    setPage(1);
   }, [coctails]);
+
+  const coctailSlice = coctails.slice((page-1) * RESULTSPERPAGE, page * RESULTSPERPAGE);
+  const pageCount = Math.ceil(coctails.length / RESULTSPERPAGE);
   
   return (
-    <Box maxWidth='700px' margin='auto' pt={3} ref={listRef}>
-      {coctails.length === 1 
-      ? <CoctailRecipe id={coctails[0].idDrink} hideMe={null}/>
-      : coctails.map(drink => <DrindThumbnail key={drink.idDrink} drink={drink} />)} 
-    </Box>
+    <>
+      <Box
+        maxWidth='700px'
+        margin='auto'
+        pt={3}
+        ref={listRef}>
+        {coctails.length === 1
+          ? <CoctailRecipe id={coctails[0].idDrink} hideMe={null} />
+          : coctailSlice.map(drink => <DrindThumbnail key={drink.idDrink} drink={drink} />)}
+      </Box>
+      {coctails.length > RESULTSPERPAGE &&
+        <Box py={3} sx={{ width: 'fit-content', margin: 'auto' }}>
+          <Pagination
+            color="secondary"
+            variant="outlined"
+            count={pageCount}
+            shape='rounded'
+            page={page}
+            onChange={(event: React.ChangeEvent<unknown>, value: number) => setPage(value)}
+            sx={{
+              "& .MuiPaginationItem-root": {
+                color: "#cbcbcb"
+              }
+            }} />
+        </Box>}
+    </>
   );
 }
 
